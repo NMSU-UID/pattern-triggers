c6c9af8648e7999b45780b6ba170df8f45c729af
sudipnmsu <sudip.hstu@gmail.com>
2021-10-03 14:19:47 -0600

    initiating project - hello world application

---
+++ app/src/main/java/nmsu/hcc/pattern_triggers/MainActivity.java
@@ -0,0 +1,14 @@
+package nmsu.hcc.pattern_triggers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
