b311602aedb46294b3f7d4876192526e44636b44
sudipnmsu <sudip.hstu@gmail.com>
2021-11-15 21:32:48 -0700

    pattern draw and show functionalities added

--- app/src/main/java/nmsu/hcc/pattern_triggers/DrawPatternActivity.java
+++ app/src/main/java/nmsu/hcc/pattern_triggers/DrawPatternActivity.java
@@ -3,9 +3,19 @@ package nmsu.hcc.pattern_triggers;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -21,10 +31,25 @@ import java.io.FileNotFoundException;
 
 public class DrawPatternActivity extends ImageActivity {
 
+    DrawingView drawingView;
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw_pattern);
+
+        imageView = findViewById(R.id.ivPatternImage);
+        drawingView = findViewById(R.id.llCanvas);
+        drawingView.getLatestBitmapImage(new LatestBitmapImageListener() {
+            @Override
+            public void latestBitmapImage(Bitmap imageBitmap) {
+                Log.e("latestBitmapImage", "bytes: "+String.valueOf(imageBitmap.getByteCount()));
+                Log.e("latestBitmapImage", "height: "+String.valueOf(imageBitmap.getHeight()));
+                imageView.setImageBitmap(Bitmap.createScaledBitmap(
+                        imageBitmap, 512, 512, false));
+            }
+        });
 
         findViewById(R.id.btnOpenGallery).setOnClickListener(view -> {
             getImageFromGallery(new ImageGetListener() {
@@ -61,4 +86,5 @@ public class DrawPatternActivity extends ImageActivity {
         });
 
     }
+
 }
