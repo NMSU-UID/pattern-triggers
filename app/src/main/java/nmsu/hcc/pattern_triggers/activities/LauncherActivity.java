81e23757883be4d1330fcf86e047d788a3b4c7d8
sudipnmsu <sudip.hstu@gmail.com>
2021-11-30 10:47:21 -0700

    launcher activity created

---
+++ app/src/main/java/nmsu/hcc/pattern_triggers/activities/LauncherActivity.java
@@ -0,0 +1,38 @@
+package nmsu.hcc.pattern_triggers.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import nmsu.hcc.pattern_triggers.LocalStorage;
+import nmsu.hcc.pattern_triggers.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher_activity);
+
+        if(LocalStorage.getInstance().getSavedFeatureMapping(this)!=null){
+            if(LocalStorage.getInstance().getSavedFeatureMapping(this).size()!=0){
+                startActivity(new Intent(LauncherActivity.this, DrawPatternActivity.class));
+                finish();
+            } else {
+                new Handler().postDelayed(() -> {
+                    Intent mainIntent = new Intent(LauncherActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }, 1500);
+            }
+        } else {
+            new Handler().postDelayed(() -> {
+                Intent mainIntent = new Intent(LauncherActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }, 1500);
+        }
+    }
+}
