b312760e6cf71dcee320974bb178c1681cdf63e4
sudipnmsu <sudip.hstu@gmail.com>
2021-12-04 14:42:12 -0700

    landing page created

--- app/src/main/java/nmsu/hcc/pattern_triggers/activities/MainActivity.java
+++ app/src/main/java/nmsu/hcc/pattern_triggers/activities/MainActivity.java
@@ -1,30 +1,48 @@
 package nmsu.hcc.pattern_triggers.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-import nmsu.hcc.pattern_triggers.LaunchApplicationHelper;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
 import nmsu.hcc.pattern_triggers.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout llGif;
+    ImageView ivCross;
+    ImageView gifImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnSettings).setOnClickListener(view -> {
-            //LaunchApplicationHelper.openApplication(MainActivity.this, "com.sec.android.app.camera");
-            //LaunchApplicationHelper.openApplication(MainActivity.this, LaunchApplicationHelper.getCameraPackageName(MainActivity.this));
-            //LaunchApplicationHelper.switchFlashLight(MainActivity.this, true);
+        llGif = findViewById(R.id.llGif);
+        ivCross = findViewById(R.id.ivCrossBtn);
+        gifImageView = findViewById(R.id.gifView);
+
+        findViewById(R.id.tvSettings).setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, SettingsActivity.class));
         });
 
-        findViewById(R.id.btnTurnOffTorch).setOnClickListener(view -> {
-            LaunchApplicationHelper.switchFlashLight(MainActivity.this, false);
+        findViewById(R.id.tvTutorial).setOnClickListener(view -> {
+            llGif.setVisibility(View.VISIBLE);
+            Glide.with(this)
+                    .asGif()
+                    .load(R.drawable.tutotial)
+                    .into(gifImageView).clearOnDetach();
+        });
+
+        ivCross.setOnClickListener(view -> {
+            llGif.setVisibility(View.GONE);
+            Glide.with(gifImageView.getContext()).clear(gifImageView);
         });
 
     }
